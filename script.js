f269edf3d85c4e0cd5a8ee69d31d609db47ac9a9
Mihir Sahu <114948140+0xmihirsahu@users.noreply.github.com>
2023-07-09 05:16:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,180 +1,180 @@
 let web3;
 const connectWalletButton = document.getElementById("connect-wallet");
-const donationContractAddress = "0x0c4492aE1c52B9Df408489fF4a162ef9dBe6d8Df";
+const donationContractAddress = "0x813c476086887001cc4d0afF2332e199A2146380";
 const donationContractABI = [
-    {
-        inputs: [],
-        stateMutability: "nonpayable",
-        type: "constructor",
-    },
-    {
-        anonymous: false,
-        inputs: [
-            {
-                indexed: true,
-                internalType: "string",
-                name: "domain",
-                type: "string",
-            },
-            {
-                indexed: true,
-                internalType: "address",
-                name: "donor",
-                type: "address",
-            },
-            {
-                indexed: false,
-                internalType: "uint256",
-                name: "amount",
-                type: "uint256",
-            },
-        ],
-        name: "DonationReceived",
-        type: "event",
-    },
-    {
-        anonymous: false,
-        inputs: [
-            {
-                indexed: true,
-                internalType: "string",
-                name: "domain",
-                type: "string",
-            },
-            {
-                indexed: true,
-                internalType: "address",
-                name: "ngoAddress",
-                type: "address",
-            },
-        ],
-        name: "NGORegistered",
-        type: "event",
-    },
-    {
-        inputs: [
-            {
-                internalType: "string",
-                name: "",
-                type: "string",
-            },
-        ],
-        name: "domainBalance",
-        outputs: [
-            {
-                internalType: "uint256",
-                name: "",
-                type: "uint256",
-            },
-        ],
-        stateMutability: "view",
-        type: "function",
-    },
-    {
-        inputs: [],
-        name: "domainDonationThreshold",
-        outputs: [
-            {
-                internalType: "uint256",
-                name: "",
-                type: "uint256",
-            },
-        ],
-        stateMutability: "view",
-        type: "function",
-    },
-    {
-        inputs: [
-            {
-                internalType: "string",
-                name: "domain",
-                type: "string",
-            },
-        ],
-        name: "donate",
-        outputs: [],
-        stateMutability: "payable",
-        type: "function",
-    },
-    {
-        inputs: [],
-        name: "owner",
-        outputs: [
-            {
-                internalType: "address",
-                name: "",
-                type: "address",
-            },
-        ],
-        stateMutability: "view",
-        type: "function",
-    },
-    {
-        inputs: [
-            {
-                internalType: "string",
-                name: "domain",
-                type: "string",
-            },
-            {
-                internalType: "address",
-                name: "ngoAddress",
-                type: "address",
-            },
-        ],
-        name: "registerNGO",
-        outputs: [],
-        stateMutability: "nonpayable",
-        type: "function",
-    },
-    {
-        inputs: [
-            {
-                internalType: "string",
-                name: "",
-                type: "string",
-            },
-            {
-                internalType: "uint256",
-                name: "",
-                type: "uint256",
-            },
-        ],
-        name: "registeredNGOAddresses",
-        outputs: [
-            {
-                internalType: "address",
-                name: "",
-                type: "address",
-            },
-        ],
-        stateMutability: "view",
-        type: "function",
-    },
-    {
-        inputs: [
-            {
-                internalType: "string",
-                name: "",
-                type: "string",
-            },
-            {
-                internalType: "address",
-                name: "",
-                type: "address",
-            },
-        ],
-        name: "registeredNGOs",
-        outputs: [
-            {
-                internalType: "bool",
-                name: "",
-                type: "bool",
-            },
-        ],
-        stateMutability: "view",
-        type: "function",
-    },
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "domain",
+				"type": "string"
+			}
+		],
+		"name": "donate",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "string",
+				"name": "domain",
+				"type": "string"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "donor",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "DonationReceived",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "string",
+				"name": "domain",
+				"type": "string"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "ngoAddress",
+				"type": "address"
+			}
+		],
+		"name": "NGORegistered",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "domain",
+				"type": "string"
+			},
+			{
+				"internalType": "address",
+				"name": "ngoAddress",
+				"type": "address"
+			}
+		],
+		"name": "registerNGO",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"name": "domainBalance",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "domainDonationThreshold",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "owner",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			},
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "registeredNGOAddresses",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			},
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "registeredNGOs",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
 ];
 const amountInput = document.getElementById("amount");
 const categorySelect = document.getElementById("sector");
@@ -222,7 +222,6 @@ async function interactContract() {
         throw new Error("Invalid amount");
     }
 
- 
     const accounts = await web3.eth.getAccounts();
     const userAccount = accounts[0];
 
@@ -231,14 +230,18 @@ async function interactContract() {
         donationContractAddress
     );
 
-   
     await DonationContract.methods.donate(category).send({
-  from: userAccount,
+    from: userAccount,
         value: web3.utils.toWei(amount, "ether"),
         gas: 300000,
+    })
+    .on('confirmation', function(confirmationNumber, receipt){
+        if(confirmationNumber === 1){
+            alert('Donation Successful! Here\'s the tx hash: ' + receipt.transactionHash);
+        }
     });
 
- console.log(userAccount);
+    console.log(userAccount);
 
     
 }
